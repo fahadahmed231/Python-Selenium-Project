a08cdd5c847713a73db1115f8b342c0c4ae7af06
fahadahmed231 <168786929+fahadahmed231@users.noreply.github.com>
2024-09-24 22:05:28 +0500

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,25 @@
+    from selenium import webdriver
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
+    import time
+
+    driver = webdriver.Chrome()
+    query = "laptop"
+    file = 0
+
+    for i in range(1, 20):
+        driver.get(f"https://www.amazon.in/s?k={query}&page={i}&crid=234WC15ZUST3N&sprefix=1ap%2Caps%2C432&ref=nb_sb_noss_2")
+
+        elems = driver.find_elements(By.CLASS_NAME, "puis-card-container")
+        print(f"{len(elems)} items found")
+
+        for elem in elems:
+            d = elem.get_attribute("outerHTML")
+            with open(f"data/{query}_{file}.html", "w", encoding="utf-8") as f:
+                f.write(d)
+            file += 1
+
+        # print(elem.text)
+
+    time.sleep(2)
+    driver.close() 
